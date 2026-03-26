debae8fca7ec4cbc02d7b367a30e87129ef7dfff
Kunal Sharma <kunal.lalbahadur.sharma05@gmail.com>
2026-03-26 23:18:25 +0530

    version 3

--- heart-explorer/vite.config.js
+++ heart-explorer/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+          r3f: ["@react-three/fiber", "@react-three/drei"],
+        },
+      },
+    },
+  },
+});
